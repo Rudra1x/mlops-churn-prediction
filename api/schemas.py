79bb592637e6d59c1d5680082c59986a12f90a22
Rudraksh <ranup55novo@gmail.com>
2025-12-19 14:02:05 +0530

    production-ready ML API with Dockerized inference

--- api/schemas.py
+++ api/schemas.py
@@ -9,6 +9,17 @@ class ChurnRequest(BaseModel):
     Dependents: str
     tenure: int
     PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
     MonthlyCharges: float
     TotalCharges: float
 
